c377e0b5360d522248a65385a52c9e7d8ae87dfe
DanilShabolta <danilshabolta@gmail.com>
2022-04-02 14:46:36 +0700

    init commit

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -10,15 +10,35 @@ class profile extends StatefulWidget {
 class _profileState extends State<profile> {
   @override
   Widget build(BuildContext context) {
-    final textStyle = const TextStyle(color: Colors.black, fontSize: 16);
+    final textStyle = const TextStyle(color: Colors.black, fontSize: 20);
     return Scaffold(
       body:
       Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(0),
         child: ListView(
           children: [
-            TextButton(onPressed: () {}, child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Image(image: AssetImage('images/gachi.gif')), Text('  Сдать батарейки', style: textStyle,)],)),
-            TextButton(onPressed: () {}, child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Icon(Icons.thermostat, size: 35, color: Colors.green), Text('  Сдать градусники', style: textStyle,),],)),
+            SizedBox(height: 20),
+            OutlinedButton(onPressed: () {},
+                style: OutlinedButton.styleFrom(side: BorderSide(color: Colors.green.shade300), padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15)),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Icon(Icons.lock_open, size: 35, color: Colors.green),Text('  Войти', style: textStyle,)],)),
+            SizedBox(height: 20),
+            TextButton(onPressed: () {}, style: OutlinedButton.styleFrom(side: BorderSide(color: Colors.green.shade300), padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15)),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Icon(Icons.thermostat, size: 35, color: Colors.green), Text('  Избранное', style: textStyle,),],)),
+            SizedBox(height: 20),
+            TextButton(onPressed: () {}, style: OutlinedButton.styleFrom(side: BorderSide(color: Colors.green.shade300), padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15)),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Icon(Icons.thermostat, size: 35, color: Colors.green), Text('  Добавить точку', style: textStyle,),],)),
+            SizedBox(height: 20),
+            TextButton(onPressed: () {}, style: OutlinedButton.styleFrom(side: BorderSide(color: Colors.green.shade300), padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15)),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Icon(Icons.thermostat, size: 35, color: Colors.green), Text('  О приложении', style: textStyle,),],)),
+            SizedBox(height: 20),
+            TextButton(onPressed: () {}, style: OutlinedButton.styleFrom(side: BorderSide(color: Colors.green.shade300), padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15)),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Icon(Icons.thermostat, size: 35, color: Colors.green), Text('  Политика конфиден...', style: textStyle,),],)),
+            SizedBox(height: 20),
+            TextButton(onPressed: () {}, style: OutlinedButton.styleFrom(side: BorderSide(color: Colors.green.shade300), padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15)),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Icon(Icons.thermostat, size: 35, color: Colors.green), Text('  Написать в техподдержку', style: textStyle,),],)),
+            SizedBox(height: 20),
+            TextButton(onPressed: () {}, style: OutlinedButton.styleFrom(side: BorderSide(color: Colors.green.shade300), padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15)),
+                child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [Icon(Icons.thermostat, size: 35, color: Colors.green), Text('  Оценить приложение', style: textStyle,),],)),
           ],
         ),
       ),
